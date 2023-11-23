f2c06fd76cc3bd533cd12accb96cb7c6272ffd80
gumbo <dae4805@naver.com>
2023-11-23 23:46:21 +0900

    (Project/setup) - ts 파일 공유가능하게 설정

--- ums-client/src/App.tsx
+++ ums-client/src/App.tsx
@@ -2,8 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {ShareType} from "@share/ShareType.ts";
 
 function App() {
+    const A: ShareType = {
+        a: 'a'
+    }
+    console.log(A)
   const [count, setCount] = useState(0)
 
   return (
