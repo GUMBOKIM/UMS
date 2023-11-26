8457d5c112be38f5f6d0552352ca92cf6b1d24a6
gumbo <dae4805@naver.com>
2023-11-26 12:07:28 +0900

    (Backend/feature) - share package에서 import하는 것 취소

--- ums-server/src/app.module.ts
+++ ums-server/src/app.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import configOptions from './config/config.options';
 import databaseConfig from './config/database.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(configOptions),
-    TypeOrmModule.forRootAsync(databaseConfig()),
+    // TODO: Global 환경설정 추후 추가
+    // ConfigModule.forRoot(configOptions),
+    TypeOrmModule.forRoot(databaseConfig),
   ],
   controllers: [AppController],
   providers: [AppService],
--- ums-server/src/config/database.config.ts
+++ ums-server/src/config/database.config.ts
@@ -1,32 +1,25 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Company } from '../entity/company';
 import { Part } from '../entity/part';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Member } from '../entity/member';
 import { Stock } from '../entity/stock';
 import ForwardEntities from '../entity/forward';
 import ReceiveEntities from '../entity/receive';
 
-export default () => {
-  return {
-    imports: [ConfigModule],
-    inject: [ConfigService],
-    useFactory: async (configService: ConfigService) => ({
-      type: 'mysql',
-      host: configService.get('DB_HOST'),
-      port: Number(configService.get('DB_PORT')),
-      username: configService.get('DB_USERNAME'),
-      password: configService.get('DB_PASSWORD'),
-      database: configService.get('DB_NAME'),
-    }),
-    entities: [
-      Company,
-      Part,
-      Member,
-      Stock,
-      ...ForwardEntities,
-      ...ReceiveEntities,
-    ],
-    synchronize: true,
-  } as TypeOrmModuleOptions;
-};
+export default {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'gumbo',
+  password: 'developer-gumbo',
+  database: 'union_mgmt',
+  entities: [
+    Company,
+    Part,
+    Member,
+    Stock,
+    ...ForwardEntities,
+    ...ReceiveEntities,
+  ],
+  synchronize: true,
+} as TypeOrmModuleOptions;
