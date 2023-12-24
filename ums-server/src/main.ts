c22bac05201d2a8cc195a3d2f1ecf299bd5fdde0
gumbo <dae4805@naver.com>
2023-12-24 21:24:42 +0900

    (Backend/feature) - add class validator

--- ums-server/src/main.ts
+++ ums-server/src/main.ts
@@ -1,10 +1,12 @@
+import { sessionConfig } from '@config/session.config';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { sessionConfig } from './config/session.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await sessionConfig(app);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
 }
 
